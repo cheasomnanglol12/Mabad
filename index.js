caf39f2407634a3db80febf8215651a5c8cd0bec
cheasomnanglol12 <mybrolol096228@gmail.com>
2024-08-10 22:38:39 +0700

    Update index.js

--- index.js
+++ index.js
@@ -171,7 +171,4 @@ const generateUUID = () => {
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 const delayRandom = () => Math.random() / 3 + 1;
 
-module.exports = (req, res) => {
-    bot.handleUpdate(req.body);
-    res.status(200).send('OK');
-};
+bot.launch();
